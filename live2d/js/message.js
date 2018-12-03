84835454f3f326286e45271158452473ddb0df9b
singagain <991407623@qq.com>
2018-12-03 13:11:14 +0800

    f

--- live2d/js/message.js
+++ live2d/js/message.js
@@ -280,6 +280,27 @@ if(!norunFlag){
 					showMessage('写点什么吧！',0);
 					return;
 				}
+
+	// // 获取谷歌浏览器版本
+	// function getChromeVersion() {
+	// 	var arr = navigator.userAgent.split(' '); 
+	// 	var chromeVersion = '';
+	// 	for(var i=0;i < arr.length;i++){
+	// 		if(/chrome/i.test(arr[i]))
+	// 		chromeVersion = arr[i]
+	// 	}
+	// 	if(chromeVersion){
+	// 		return Number(chromeVersion.split('/')[1].split('.')[0]);
+	// 	} else {
+	// 		return false;
+	// 	}
+	// }
+	// if(getChromeVersion()) {
+	// 	var version = getChromeVersion();
+	// 	if(version < 32) {
+	// 		alert('您使用的谷歌浏览器版本过低，为了更好地体验请将浏览器升级到最新版本！');
+	// 	}
+	// }
 				if(userid_ == ""){
 					showMessage('聊之前请告诉我你的名字吧！',0);
 					return;
